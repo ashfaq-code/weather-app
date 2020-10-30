f0260eeec1c98c911dd5228ad72c3dcf828ec82a
Ashfaq Ahmad <ashfaqcoding@gmail.com>
2020-10-31 00:15:37 +0500

    changed location to complete address in geocode

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -18,7 +18,7 @@ const geocode = (address, callback) => {
             callback(undefined, {
                 latitude: body.features[0].center[1],
                 longitude: body.features[0].center[0],
-                location: body.features[0].text,
+                location: body.features[0].place_name,
             })
         }
     })
