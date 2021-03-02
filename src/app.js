ced57cd33f1a739b373adf8f51e03a60b2688ee2
Ashfaq Ahmad <ashfaqcoding@gmail.com>
2021-03-02 18:46:38 +0500

    commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 //Start nodemon with multiple extentions will restart server on changes in mentioned extensions
-// In this file: nodemon src/app.js -e js, hbs (-e : extension)
+// In this file: nodemon src/app.js -e js, hbs (-e => extension)
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 const path = require('path')
